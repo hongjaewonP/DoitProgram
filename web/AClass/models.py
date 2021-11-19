da016edad7a924684c46b65ce1532dd1ce121eac
MinJeong1104 <68368150+MinJeong1104@users.noreply.github.com>
2021-11-20 00:58:27 +0900

    Add django server

---
+++ web/AClass/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class Class(models.Model):
+    number = models.CharField(max_length=200)
+    subnum = models.CharField(max_length=50)
+    title = models.CharField(max_length=200)
