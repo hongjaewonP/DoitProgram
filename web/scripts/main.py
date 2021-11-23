5c7921ffde56cd2bd8d8602f0e6e0e974b71e0f1
MinJeong1104 <68368150+MinJeong1104@users.noreply.github.com>
2021-11-24 05:30:19 +0900

    Update main.py

--- web/scripts/main.py
+++ web/scripts/main.py
@@ -9,7 +9,7 @@ def run():
     url = "https://eureka.ewha.ac.kr/eureka/my/public.do?pgId=P531005519"
     op=Options()
     op.add_experimental_option('prefs',{'download.default_directory':r'C:\Users\mocar\Desktop\down'})
-    browser = webdriver.Chrome(options=op, executable_path=r'C:\Users\mocar\PycharmProjects\Project\web\scripts\chromedriver.exe')
+    browser = webdriver.Chrome()
     browser.get(url)
     browser.implicitly_wait(2)
 
