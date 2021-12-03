49044ce6b35f98547f0809a2547b04c4236da4e7
MinJeong1104 <68368150+MinJeong1104@users.noreply.github.com>
2021-12-03 18:33:46 +0900

    Update models.py

--- web/AClass/models.py
+++ web/AClass/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Class(models.Model):
-    number = models.CharField(max_length=200)
-    subnum = models.CharField(max_length=50)
-    title = models.CharField(max_length=200)
+    className = models.CharField(max_length=200)
+    classNum= models.CharField(max_length=50)
+    professor = models.CharField(max_length=200)
