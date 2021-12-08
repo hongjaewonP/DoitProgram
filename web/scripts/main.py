f063cd1117373fdd39c49ed77d49fd866eb60c6a
MinJeong1104 <68368150+MinJeong1104@users.noreply.github.com>
2021-12-08 21:06:21 +0900

    기말제출용

--- web/scripts/main.py
+++ web/scripts/main.py
@@ -1,270 +1,75 @@
-from bs4 import BeautifulSoup  # cmd의 pip명령을 통해 따로 깔아주어야 해요
-from selenium import webdriver  # 이것두! webdriver은 따로 설치해야 합니다
+#크롤링 호환을 위한 초기버전 test
+from bs4 import BeautifulSoup
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.action_chains import ActionChains       #새탭클릭
-from selenium.webdriver.common.keys import Keys
-from PIL import Image #pip install pillow
-from Screenshot import Screenshot_Clipping #pip install selenium_screenshot
 import time
-import os
-import shutil
+from datetime import datetime
 from AClass.models import Class
-
-def run():  
-    
-    url = "https://eureka.ewha.ac.kr/eureka/my/public.do?pgId=P531005519"  # 강의계획안 사이트입니다.
-    url2 = "http://localhost:8000/"
-
-    print("2021 - 겨울계절1차 : 1, 2021 - 2학기 : 2, ... , 2015 - 1학기 : 28 \n 반드시 숫자로만 입력해야 합니다! \n")
-    semesterNum = input("학기를 설정해주세요. (1~28) : ")
-
-    # 크롬 브라우저로 웹 실행
-    path = "/usr/local/bin/chromedriver"
-    downloadPath = r'D:\학교\3학년 2학기\캡스톤디자인프로젝트\구현\다운로드 폴더'
-    # 경로는 ''안에 넣어주면 되어요
-
-
-    #파일 다운로드 경로 변경
-    op = Options()
-    op.add_experimental_option('prefs',{
-        'download.default_directory': downloadPath
-    })
-    #최대화면 변경
-    op.add_argument('--start-fullscreen')
+def run():
+    url = "https://eureka.ewha.ac.kr/eureka/my/public.do?pgId=P531005519"
+    op=Options()
+    op.add_experimental_option('prefs',{'download.default_directory':r'C:\Users\mocar\Desktop\down'})
     op.add_argument('--headless')
     op.add_argument('--no-sandbox')
     op.add_argument('--disable-dev-shm-usage')
     op.add_argument('blink-settings=imagesEnabled=false')
     op.add_argument('--disable-gpu')
-    # 드라이버 실행
-    driver = webdriver.Chrome(path, chrome_options= op)
-    driver.get(url)
-    time.sleep(2)
+    op.add_argument("--remote-debugging-port=9222")
+    op.add_experimental_option("excludeSwitches", ["enable-automation"])
+    op.add_experimental_option('useAutomationExtension', False)
+    browser = webdriver.Chrome('/usr/local/bin/chromedriver')
+    browser = webdriver.Chrome(options=op,executable_path='/usr/local/bin/chromedriver')
+    browser.get(url)
+    browser.implicitly_wait(2)
 
-    # 조회를 위한 조건문을 채워야 함
-
-
-    #학기 선택하기
-    # 학기 선택은 상하키로 조종할 수 있습니다. 위에서부터 내려갈수록 반복 횟수를 늘려요
-    # 키입력 참고 주소 : https://coding-kindergarten.tistory.com/151
-
-    for i in range(1, int(semesterNum)):
-
-        driver.find_element_by_xpath('//*[@id="mainframe_VFrameSet_WorkFrame_Child__form_div_Work_div_search_cbbYearTermCd_comboedit_input"]'
-                                 ).send_keys(Keys.ARROW_DOWN)
-
-    '''
-    select option(drop down)을 선택할 때 그냥 xpath가 아니라 full xpath를 복사해야 기능합니다.
-    통상적이지 않은 input based dropdown이라 많이 헤멨네요...ㅠ
-    element 찾는 법은 ctrl+F로 검색하면 빠릅니다. ---> 스크롤이 생기는 dropdown의 경우 스크롤 코드를 추가해야하기 때문에 down key로 변경해서 구현하였습니다.
-    혹시 나중에 쓰일 일이 있을까봐 주석으로 남겨둬요!
-    '''
-    #교과목 구분 설정하기
-    # 귀찮게 설정하는 이유 : 교과목이 너무 많아서 사이트 자체에서 조건을 하나 이상 설정하게끔 막아놨습니다. 스크롤이 터지는 걸 방지하기 위한 것 같아요
-    # 교과목 구분에 따라 교양/비사대교직/전공선택(교직)/대학기초/전공기초/전공 검색 후 합치기
-    # 학점교환의 경우 폼이 달라서 나중에 추가하기로 했어요! 애초에 강계가 없어서 db만 있으면 될 것 같은
-
-    # 교과목 구분 combobox 클릭하기
-    for i in range(1, 7):
-        # 교과목 구분 combobox 클릭하기
-        driver.find_element_by_xpath(
-            '//*[@id="mainframe_VFrameSet_WorkFrame_Child__form_div_Work_div_search_cbbViewKindCd_comboedit_input"]'
-                                     ).click()
-        #아래키 클릭
-        driver.find_element_by_xpath(
-            '//*[@id="mainframe_VFrameSet_WorkFrame_Child__form_div_Work_div_search_cbbViewKindCd_comboedit_input"]'
-            ).send_keys(Keys.ARROW_DOWN)
-        # 엔터 치기
-        driver.find_element_by_xpath(
-            '//*[@id="mainframe_VFrameSet_WorkFrame_Child__form_div_Work_div_search_cbbViewKindCd_comboedit_input"]'
-            ).send_keys(Keys.ENTER)
-
-        # 검색버튼 누르기
-        btn = driver.find_element_by_xpath(
-            "//*[@id='mainframe_VFrameSet_WorkFrame_Child__form_div_Work_div_search_btnSearchTextBoxElement']/div")
-        btn.click()
-        time.sleep(3)
-
-        #현재 html 가져오기
-        soup = BeautifulSoup(driver.page_source, "html.parser")
-        #print(soup.text)    #줄줄줄 출력됨
-        #print(soup.prettify())  #html이 이쁘게 출력됨
-
-        # 해당 검색결과가 없을 경우 스루할 수 있는 코드가 필요
-        if soup.find("div", id = 'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_0_cell_0_0') == None:
-            pass
-        else:
-             # 만약 조회 결과가 존재할 경우, 마지막까지 다운로드 버튼을 누름
-             # 0 : 순번 | 1 : 학수번호 | 2 : 분반 | 3 : 교과목명
-             # 4 : 교과목구분 | 5 : 교과영역 | 6 : 개설 학과 | 7 : 학년 | 8 : 교수명
-             # 9 : 학점 | 10 : 시간 | 11 : 요일 | 12 : 교시 | 13 : 교실
-             # 14 : 국문 | 15 : 영문 | 16 : FILE | 17 : URL
-             # 18 : 영어강의 | 19 : 원어강의 | 20 : 원격강의 | 21 : 인문학관련교양과목 | 22 : SW과목
-             # 23 : 정원 | 24 : 수업방식 | 25 : 비고
-             '''
-             파일명 : 교과목명_분반_교수_강의계획안번호
-             '''
-
-             #파일 처음부터 끝까지 받기
-             Temp = 1
-             count = 0
-             Total = 0
-             tempText = driver.find_element_by_id('mainframe_VFrameSet_WorkFrame_Child__form_div_Work_stcTextTextBoxElement').text
-             if "총건수" in tempText:
-                 tempList = tempText.split(':')
-                 tempText = tempList[1].replace(' ','')
-                 tempText = tempText.replace(']','')
-                 tempText = int(tempText)
-                 print("Total : " + str(tempText))
-
-             while(Temp == 1):
-                 Total +=1
-                 if (Total > tempText):
-                     print("다 돌았다\n")
-                     break
-                 if soup.find("div",id ='mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_'+str(count)+'_cell_'+str(count)+'_0') == None:
-                     print("이 행이 없는 것 같다\n")
-                     break
-                 else:
-                     #어떻게 이름을 저장할지 미리 정해둠
-                     className = driver.find_element_by_id(
-                         'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_' + str(
-                             count) + '_cell_' + str(count) + '_3').text
-                     classNum = driver.find_element_by_id(
-                         'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_' + str(
-                             count) + '_cell_' + str(count) + '_2').text
-                     professor = driver.find_element_by_id(
-                         'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_' + str(
-                             count) + '_cell_' + str(count) + '_8').text
-                     #가끔 교수명에 \n가 들어가는 경우가 있어서... 치환
-                     #예외 > \, /, :, *, <, >, | 강의명에 포함되면 안됨ㅠ
-                     className = className.replace("/","+")
-                     className = className.replace("\\", "+")
-                     className = className.replace(":", "-")
-                     className = className.replace("*", "+")
-                     className = className.replace("<", "[")
-                     className = className.replace(">", "]")
-                     className = className.replace("|", "+")
-                     className = className.replace("\n", "")
-                     professor = professor.replace("/","+")
-                     professor = professor.replace("\\", "+")
-                     professor = professor.replace(":", "-")
-                     professor = professor.replace("*", "+")
-                     professor = professor.replace("<", "[")
-                     professor = professor.replace(">", "]")
-                     professor = professor.replace("|", "+")
-                     professor = professor.replace("\n","")
-                     new_filename = className + "_" + classNum + "_" + professor + "_"
-
-                     print("count: "+str(count))
-
-                     # 하나의 강의에 들은 모든 파일 받기
-
-                     for number in range(14, 18):
-
-                         # 현재 html 가져오기 이걸 넣어야 find가 제대로 작동함
-                         soup = BeautifulSoup(driver.page_source, "html.parser")
-
-                         if soup.find('div', id = 'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_'
-                                                  +str(count)+'_cell_'+str(count)+'_'+str(number)+'_controlexpand') == None:
-                             print(str(number)+" 칸이 비었다 \n")
-                             pass
-
-                         else:
-
-                             # 파일 다운 버튼을 클릭
-                             # 14 15일 경우 돋보기 16일 경우 파일 17일 경우 모르겠다... 올린 분이 안계시는 것 같애요
-                             number = str(number)
-
-                             if number == "14" or number =="15":
-                                 print("돋보기 칸 찾았다\n")
-                                 target = driver.find_element_by_id(
-                                    'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_' + str(
-                                        count) + '_cell_' + str(count) + '_' + number)
-                                 ActionChains(driver).key_down(Keys.CONTROL).click(target).key_up(Keys.CONTROL).perform()
-                                 time.sleep(2)
-                                 driver.switch_to.window(driver.window_handles[1])
-
-                                 ss = Screenshot_Clipping.Screenshot()
-                                 ss.full_Screenshot(driver, save_path= downloadPath, image_name= r"\temp.png")
-                                 # 하단이 살짝 잘려서 저장되는데 이유를 모르겠네요ㅠㅠ
-
-                                 driver.close()
-                                 driver.switch_to.window(driver.window_handles[0])
-
-                             if number == "16":
-                                 print("파일 칸 찾았다\n")
-                                 driver.find_element_by_id(
-                                    'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_' + str(
-                                        count) + '_cell_' + str(count) + '_' + number).click()
-
-                             if number == "17" :
-                                print("URL 찾았다~ \n")
-
-
-                             time.sleep(2)  # 없으면 오류남 (파일이 제대로 다운이 안되거나...)
-
-                             # 파일명 변경은 selenium에서 할 수 없기에 따로 코드를 짜야 한다.
-
-                             filename = max([downloadPath + '\\' + f for f in os.listdir(downloadPath)],
-                                            key=os.path.getctime)
-                             temp = 1
-
-                             while (temp == 1):
-                                 if "crdownload" in filename:  # 아직 저장이 덜 되었을 때 생김
-                                     temp = 1
-                                 else:
-                                     temp = 0
-
-                             print(filename)
-                             list_filename = filename.split(".")
-                             lenmax = len(list_filename)
-                             fileEx = list_filename[lenmax-1]
-                             print(new_filename + number + "." + fileEx)
-                             shutil.move(os.path.join(downloadPath, filename),
-                                         os.path.join(downloadPath, new_filename + number + "." + fileEx))
-                             time.sleep(1)
-
-                    # 브라우저 스크롤 가장 밑으로
-
-                     attr = {'id': 'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_vscrollbar_incbuttonAlignImageElement',
-                             'style': 'user-select: none; position: absolute; left: 0px; top: 0px; width: 15px; height: 15px; background-repeat: no-repeat; background-position: center center; background-image: url("/eureka/my/nxc/_theme_/theme/base/scr_WF_VerincN.png");'}
-                     if soup.find('div', attrs=attr) != None:
-                        for i in range(0, 4):
-                            driver.find_element_by_id('mainframe_VFrameSet_WorkFrame_Child__form_div_Work_vscrollbar_incbuttonAlignImageElement').click()
-
-
-                     # 스크롤 1칸 내리기
-
-                     attr = {'id':'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_vscrollbar_incbutton', 'style' : 'user-select: none; position: absolute; overflow: hidden; left: 0px; top: 478px; width: 15px; height: 15px; border-radius: 0px; background-color: transparent; cursor: pointer;'}
-
-                     if soup.find('div', attrs = attr) != None:
-                         driver.find_element_by_id(
-                             'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_vscrollbar_incbuttonAlignImageElement').click()
-
-
-                     count = (count+1)%18
-
-             # 다운로드가 끝났으면 다시 최상단으로 스크롤을 돌려야 함
-             scrollStyle = driver.find_element_by_id(
-                'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_vscrollbar_trackbar').get_attribute('style')
-             list_scrollStyle = scrollStyle.split()
-             scrollStyleHeight = list_scrollStyle[16]
-
-             while (scrollStyleHeight != '15px,'):
-                     # 가장 상단에 있을 경우 값은 15px
-                driver.find_element_by_id(
-                    'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_vscrollbar_decbuttonAlignImageElement').click()
-                scrollStyle = driver.find_element_by_id(
-                    'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_vscrollbar_trackbar').get_attribute('style')
-                list_scrollStyle = scrollStyle.split()
-                scrollStyleHeight = list_scrollStyle[16]
-
-             time.sleep(2)
-
-    print("완료")
-    time.sleep(100)
-
-    Class(className=className, classNum=classNum, professor=professor).save()
-    print(className,classNum,professor)
+    # 미리 조건 설정 (일단 대충했어요 테스트용!! 입력이나 체크 안할거면 그냥 엔터치심 되어요)
+    print("맞으면 1")
+    qEng = input("영어강의인가요? : ")
+    qRem = input("원격강의인가요? : ")
+    print("\n 단답형")
+    classNum = input("학수번호? : ")
+    classN = input("강의명? : ")
+    # 영어강의 체크박스 체크.
+    if qEng == "1":
+        englishCheck = browser.find_element_by_id(
+            "mainframe_VFrameSet_WorkFrame_Child__form_div_Work_div_search_ckbEngChk_chkimg")
+        englishCheck.click()
+    else:
+        print()
+    # 원격강의 체크박스 체크.
+    if qRem == "1":
+        remoteCheck = browser.find_element_by_id(
+            "mainframe_VFrameSet_WorkFrame_Child__form_div_Work_div_search_ckbOnChk_chkimg")
+        remoteCheck.click()
+    else:
+        print()
+    # 학수번호 text box
+    className = browser.find_element_by_id(
+        'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_div_search_ipbSubjectCd_input')
+    className.click()  # 클릭을 하지 않고 send keys를 사용하면 오류가 납니다...  ㅠㅠ 한참 헤맸네요 click 필수!!
+    className.send_keys(classNum)
+    time.sleep(3)  # sleep을 쓰지 않으면 selenium이 개복치처럼 종료되기 때문에 시간을 줍니다
+    # 그래서 좀 시간이 걸릴 수 있어요! 기다리면 결과가 나올거에요
+    # 만약 갑자기 꺼지는 오류가 계속 난다면 sleep의 값을 좀 더 늘려주면 됩니다!
+    # 교과목명 text box
+    className = browser.find_element_by_id(
+        'mainframe_VFrameSet_WorkFrame_Child__form_div_Work_div_search_ipbSubjectNm_input')
+    className.click()
+    className.send_keys(classN)
+    time.sleep(3)
+    btn = browser.find_element_by_xpath(
+        "//*[@id='mainframe_VFrameSet_WorkFrame_Child__form_div_Work_div_search_btnSearchTextBoxElement']/div")
+    btn.click()
+    time.sleep(3)
+    soup = BeautifulSoup(browser.page_source, "html.parser")
+    # print(soup.text)    #줄줄줄 출력됨
+    # print(soup.prettify())  #html이 이쁘게 출력됨
+    number = soup.find('div', id="mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_0_cell_0_1GridCellTextContainerElement").text
+    subnum = soup.find('div', id="mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_0_cell_0_2GridCellTextContainerElement").text
+    title = soup.find('div', id="mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_0_cell_0_3GridCellTextContainerElement").text
+    div_course= soup.find('div', id="mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_0_cell_0_4GridCellTextContainerElement").text
+    professor=soup.find('div',id="mainframe_VFrameSet_WorkFrame_Child__form_div_Work_grxMain_body_gridrow_0_cell_0_8GridCellTextContainerElement").text
     
+    crawled_time=datetime.now()
+    Class(number=number, subnum=subnum, title=title, div_course=div_course, professor=professor, crawled_time=crawled_time).save()
+    print(number, subnum, title, div_course, professor)
