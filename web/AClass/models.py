f2e11c58e66dc7b07a20ef8a05f6d3ce31252189
MinJeong1104 <68368150+MinJeong1104@users.noreply.github.com>
2021-12-08 21:08:45 +0900

    기말제출용

--- web/AClass/models.py
+++ web/AClass/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Class(models.Model):
-    className = models.CharField(max_length=200)
-    classNum= models.CharField(max_length=50)
+    number = models.CharField(max_length=200)
+    subnum= models.CharField(max_length=50)
+    title = models.CharField(max_length=200)
+    div_course= models.CharField(max_length=100)
     professor = models.CharField(max_length=200)
+    crawled_time = models.CharField(max_length=200)
