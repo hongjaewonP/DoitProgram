cb42abbb4e577fd4af8524fba2e9f2b4477d4b54
rainvi <89857287+rainvi@users.noreply.github.com>
2022-01-12 06:27:39 +0900

    Update new_main.py

--- web/scripts/new_main.py
+++ web/scripts/new_main.py
@@ -1,11 +1,9 @@
+#! /usr/bin/env python
 '''
 2022-01-12 수정사항 : 서버 연동이 원활하게 되지 않을까해서 linux환경에서 돌아가도록 기존 코드를 수정했습니다. 
 제가 ubuntu로 돌려봤을 땐 잘 돌아갔는데 ec2 가상환경에선 어떨지 모르겠네요.
 헤더를 불러오는 방법도 계속 알아보고 있는데 지난 학기와 마찬가지 이유로 찾을 수가 없네요ㅠㅠ
-'''
 
-#! /usr/bin/env python
-'''
 이 코드는 강의계획안 사이트에서 파일을 자동으로 다운로드하는 코드입니다.
 설치해야하는 것 :
 beautifulsoup, selenium, pillow, selenium_screenshot
