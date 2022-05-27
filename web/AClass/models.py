e55356e1ac634480efa151397b65ea6debecbc54
MinJeong1104 <68368150+MinJeong1104@users.noreply.github.com>
2022-05-27 10:27:51 +0900

    Update models.py

--- web/AClass/models.py
+++ web/AClass/models.py
@@ -1,24 +1,16 @@
-<<<<<<< HEAD
-
-from django.db import models
-
-# Create your models here.
-class Class(models.Model):
-    number = models.CharField(max_length=200)
-    subnum = models.CharField(max_length=50)
-    title = models.CharField(max_length=200)
-=======
-from django.db import models
-
-# Create your models here.
-#강의에 대한 class 를 정의합니다. 기본적으로 charField로 선언하였습니다
-#number = 학수번호, subnum = 분반, title=강의명 div_course=교과목구분, professor=교수명, crawled_time=크롤링된 시간
-
 class Class(models.Model):
     number = models.CharField(max_length=200,null=True,default='')
+    title = models.CharField(max_length=200, null=True, default='')
     subnum= models.CharField(max_length=50,null=True,default='')
-    title = models.CharField(max_length=200,null=True,default='')
-    div_course= models.CharField(max_length=100,null=True,default='')
-    professor = models.CharField(max_length=200,null=True,default='')
-    crawled_time = models.CharField(max_length=200,null=True,default='')
->>>>>>> b9361f4fac4f82056a1adb917ecf64e59ecba63c
+    professor = models.CharField(max_length=200, null=True, default='')
+    downloadPath = models.CharField(max_length=200, null=True, default='')
+    filename = models.CharField(max_length=200, null=True, default='')
+    crawled_time = models.DateTimeField(auto_now=True,null=True)
+    weeks = models.CharField(max_length=50,null=True,default='')
+    days = models.CharField(max_length=50,null=True,default='')
+    Activities = models.CharField(max_length=50,null=True,default='')
+    Ranges = models.CharField(max_length=50,null=True,default='')
+    Materials = models.CharField(max_length=50,null=True,default='')
+    Assignments = models.CharField(max_length=50,null=True,default='')
+
+
