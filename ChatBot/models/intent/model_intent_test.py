319f192220b5ab31df93138a89d9e1a488f39cf3
rainvi <89857287+rainvi@users.noreply.github.com>
2022-06-07 13:30:42 +0900

    Update model_intent_test.py

--- ChatBot/models/intent/model_intent_test.py
+++ ChatBot/models/intent/model_intent_test.py
@@ -1,5 +1,7 @@
 from utils.chatbotPreprocessing import Preprocess
 from models.intent.intentModel import IntentModel
+from models.intent.returnQuestion import returnQ
+from models.intent.returnAnswer import returnAnswer
 
 p = Preprocess(word2index_dic=r'D:\school\team_project\chatBot\chatbotEngine\train_tools\dict\chatbot_dict.bin',
                userdic=r'D:\school\team_project\chatBot\chatbotEngine\utils\user_dict.tsv')
@@ -7,10 +9,12 @@ p = Preprocess(word2index_dic=r'D:\school\team_project\chatBot\chatbotEngine\tra
 intent = IntentModel(model_name=r'D:\school\team_project\chatBot\chatbotEngine\models\intent\intent_model.h5', proprocess= p)
 
 
-query = input("무엇이 궁금한가요? : \n")
+query = returnQ()
 predict = intent.predict_class(query)
 predict_label = intent.labels[predict]
 
 print(query)
 print("의도 예측 클래스 : ", predict)
 print("의도 예측 라벨 : ", predict_label)
+
+print(returnAnswer.returnA(predict))
