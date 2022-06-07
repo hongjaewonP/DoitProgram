1068f2a3af1787aaa215d569558fac2b47f94c74
HellenHong <hongjw030@naver.com>
2022-06-07 14:36:54 +0900

    서버 연결만 안되는 중...

--- NLPprogram/Extract/linkToServer.py
+++ NLPprogram/Extract/linkToServer.py
@@ -3,17 +3,20 @@ import os
 import paramiko
 
 
-host = 'applan.cryaditlm9jj.us-east-2.rds.amazonaws.com'
-username = 'start23'
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect(host, username=username, key_filename='key파일명.pem')
-sftp = ssh.open_sftp()
+host = '10.200.13.243'
+port = 22
 
-localpath = 'C:/Users/wonai/mystatus/Doit_program/DoitProgram/NLPprogram/Extract/dateInfo.csv'
-filepath = ''
+transprot = paramiko.transport.Transport(host,port)
+userId = "start23"  # example
+password = 'ewhastart23' # example
 
-sftp.put(localpath, filepath)  # 파일 업로드
+# 연결
+transprot.connect(username = userId, password = password)
+sftp = paramiko.SFTPClient.from_transport(transprot)
+
+# Upload - 파일 업로드
+remotepath = 'remote_test_file.csv' # sftp에 업로드 될때 파일 경로와 파일이름(이렇게 저장이 됨)을 써줍니다.
+localpath  = 'C:/Users/wonai/mystatus/Doit_program/DoitProgram/NLPprogram/Extract/dayInfo.csv' # local피시의 파일 경로와 파일이름(pc에 저장되어있는 파일이름)을 써줍니다.
+sftp.put(localpath, remotepath)
 
 sftp.close()
-ssh.close()
