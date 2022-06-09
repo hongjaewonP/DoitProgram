72744d902653c02ae6f84a69e2fcf9b43c0bc03d
MinJeong1104 <68368150+MinJeong1104@users.noreply.github.com>
2022-06-10 07:25:49 +0900

    Update views.py

--- web/AClass/views.py
+++ web/AClass/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Todo
 from .models import Class
 from rest_framework import viewsets
 from .serializers import ClassSerializers
@@ -15,42 +16,42 @@ def index(requests):
 
 #viewset은 여러 API 기능을 통합해 하나의 set로 제공합니다. 
 class ClassViewSet(viewsets.ModelViewSet):
-    queryset = Class.objects.all()
+    queryset = Todo.objects.all()
     serializer_class = ClassSerializers
 
 #일반적인 강의 조회(GET)/생성(POST)입니다.
 @csrf_exempt
 def class_info(request):
     if request.method == 'GET':
-        queryset = Class.objects.all()
+        queryset = Todo.objects.all()
         serializer = ClassSerializers(queryset, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return JsonResponse(serializer.data, json_dumps_params={'ensure_ascii': False}, safe=False)
 
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = ClassSerializers(data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            return JsonResponse(serializer.data, json_dumps_params={'ensure_ascii': False}, status=201)
         return JsonResponse(serializer.errors, status=400)
 
 
 #특정 id의 강의 조회 / 수정입니다.
 @csrf_exempt
-def class_action(request, pk):
+def class_action(request, idnums):
 
-    obj = Class.objects.get(pk=pk)
+    obj = Todo.objects.filter(idnums = idnums);
 
     if request.method == 'GET':
-        serializer = ClassSerializers(obj)
-        return JsonResponse(serializer.data, safe=False)
+        serializer = ClassSerializers(obj, many=True)
+        return JsonResponse(serializer.data, json_dumps_params={'ensure_ascii': False}, safe=False)
 
     elif request.method == 'PUT':
         data = JSONParser().parse(request)
         serializer = ClassSerializers(obj, data=data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
+            return JsonResponse(serializer.data, json_dumps_params={'ensure_ascii': False}, status=201)
         return JsonResponse(serializer.errors, status=400)
 
     elif request.method == 'DELETE':
